4def97ec859993915c453e881f00178b414e4cf6
Sudilovskaya <sudilovskaya890@gmail.com>
2022-01-23 23:30:28 +0700

    added tabs

--- scripts/tabs.js
+++ scripts/tabs.js
@@ -1,4 +1,35 @@
 const tabs = document.querySelectorAll('.card-detail__change')
+const tabsTitle = document.querySelector('.card-details__title')
+const tabsPrice = document.querySelector('.card-details__price')
+const tabsImage = document.querySelector('.card__image_item')
+
+const tabsOptions = [
+  {
+    name: "Graphite",
+    memory: '128',
+    price: 60000,
+    image: 'img/iPhone-graphite.webp'
+  },
+   {
+    name: "Silver",
+    memory: '256',
+    price: 70000,
+    image: 'img/iPhone-silver.webp'
+  }, 
+  {
+    name: "Sierra Blue",
+    memory: '512',
+    price: 80000,
+    image: 'img/iPhone-sierra_blue.webp'
+  }
+]
+
+const changeContent = (index) => {
+  tabsTitle.textContent = `Смартфон Apple iPhone 13 Pro ${tabsOptions[index].memory}GB ${tabsOptions[index].name}`
+  tabsPrice.textContent = `${tabsOptions[index].price}P`
+  
+  tabsImage.setAttribute('src', tabsOptions[index].image )
+}
 
 const changeActiveTabs = (indexClickedTab) => {  //перебор кнопок
   tabs.forEach((tab, index) => {
@@ -16,3 +47,5 @@ tabs.forEach((tab, index) => {
     changeActiveTabs(index)
   }) 
 })
+
+changeContent(0)
